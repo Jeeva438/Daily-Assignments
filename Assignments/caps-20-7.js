9728bd8a535e8ea600d8b814a812fff964e09c76
Jeeva438 <131479410+Jeeva438@users.noreply.github.com>
2023-07-20 17:15:33 +0530

    Add files via upload

---
+++ Assignments/caps-20-7.js
@@ -0,0 +1,5 @@
+var userName = "My spirit animal is panda."
+console.log(userName.replace("panda" , "PANDA"));
+
+var details = "Always in love with sunsets , flowers , moon and stars !! "
+console.log(details.slice(0 , 20) + details.slice(20 , 59).toLocaleUpperCase());
